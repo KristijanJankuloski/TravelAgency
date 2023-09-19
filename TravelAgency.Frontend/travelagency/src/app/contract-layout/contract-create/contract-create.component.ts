bc9fc1d495f5f42d6897690bbf396a5f1928e445
Kristijan <jankuloski.k@gmail.com>
2023-09-19 04:40:59 +0200

    making contract create form

--- TravelAgency.Frontend/travelagency/src/app/contract-layout/contract-create/contract-create.component.ts
+++ TravelAgency.Frontend/travelagency/src/app/contract-layout/contract-create/contract-create.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { AgencyListModel } from 'src/app/shared/models/agency';
+import { ContractCreateModel } from 'src/app/shared/models/contract';
+import { PassengerCreateModel } from 'src/app/shared/models/passenger';
 import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
@@ -10,11 +12,15 @@ import { ApiService } from 'src/app/shared/services/api.service';
 })
 export class ContractCreateComponent {
   agenciesList: AgencyListModel[];
+  roomTypes = ["Студио"];
+  transportationTypes = ["Авионски", "Автобуски", "Сопствен"];
+  serviceTypes = ["Полу-пансион", "Полн-пансион", "All-inclusive"];
+  selectedAgency?: AgencyListModel;
   createForm = new FormGroup({
     email: new FormControl(''),
     phoneNumber: new FormControl(''),
-    startDate: new FormControl(Date.now),
-    endDate: new FormControl(Date.now),
+    startDate: new FormControl(),
+    endDate: new FormControl(),
     totalPrice: new FormControl(0),
     ammountPaid: new FormControl(0),
     agencyId: new FormControl(0),
@@ -28,8 +34,8 @@ export class ContractCreateComponent {
       firstName: new FormControl(''),
       lastName: new FormControl(''),
       passportNumber: new FormControl(''),
-      passportExpirationDate: new FormControl(Date.now),
-      birthDate: new FormControl(Date.now),
+      passportExpirationDate: new FormControl(),
+      birthDate: new FormControl(),
       email: new FormControl(''),
       phoneNumber: new FormControl(''),
       address: new FormControl(''),
@@ -47,6 +53,10 @@ export class ContractCreateComponent {
     this.api.getAgenciesList().subscribe(data => {
       this.agenciesList = [...data];
     })
+  }
+
+  agencySelected() {
+    this.selectedAgency = this.agenciesList.find(x => x.id === this.createForm.value.agencyId);
   }
 
   addPassenger(){
@@ -72,6 +82,38 @@ export class ContractCreateComponent {
   }
 
   contractFormSubmit(){
-
+    const values = this.createForm.value;
+    const request: ContractCreateModel = {
+      email: values.email!,
+      phoneNumber: values.phoneNumber!,
+      startDate: values.startDate,
+      endDate: values.endDate,
+      totalPrice: values.totalPrice!,
+      ammountPaid: values.ammountPaid!,
+      plan: {
+        agencyId: values.agencyId!,
+        hotelName: values.hotelName!,
+        location: values.location!,
+        country: values.country!
+      },
+      roomType: values.roomType!,
+      serviceType: values.serviceType!,
+      transportationType: values.transportationType!,
+      passengers: values.passengers?.map(passenger => {
+        let mapped: PassengerCreateModel = { 
+          firstName: passenger.firstName!, 
+          lastName: passenger.lastName!,
+          passportNumber: passenger.passportNumber!,
+          passportExpirationDate: passenger.passportExpirationDate,
+          birthDate: passenger.birthDate,
+          email: passenger.email!,
+          address: passenger.address!,
+          phoneNumber: passenger.phoneNumber!,
+          comment: passenger.comment!
+        }
+        return mapped;
+    })!
+    }
+    console.log(request);
   }
 }
