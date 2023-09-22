1a0a1c0035c3e76f37537ad2865bd256543623b0
Kristijan <jankuloski.k@gmail.com>
2023-09-22 03:42:46 +0200

    updated currency format

--- TravelAgency.Frontend/travelagency/src/app/contract-layout/contract-print-dialog/contract-print-dialog.component.ts
+++ TravelAgency.Frontend/travelagency/src/app/contract-layout/contract-print-dialog/contract-print-dialog.component.ts
@@ -30,4 +30,15 @@ export class ContractPrintDialogComponent {
   close() {
     this._dialogRef.close();
   }
-}
+
+  convertCurrency(num: number){
+    let str = num.toFixed(2).split('.');
+    if (str[0].length >= 4) {
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    if (str[1] && str[1].length >= 5) {
+        str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return str.join('.');
+  }
+} 
